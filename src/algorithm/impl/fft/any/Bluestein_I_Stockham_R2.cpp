16cc71648e1fe76769780d7796fb327b1bd9946e
Andrey Markov <markov_av@atb.su>
2025-06-26 04:51:15 +1000

    Add Bluestein multithreading

--- src/algorithm/impl/fft/any/Bluestein_I_Stockham_R2.cpp
+++ src/algorithm/impl/fft/any/Bluestein_I_Stockham_R2.cpp
@@ -3,52 +3,70 @@
 #include <cmath>
 #include <thread>
 
-#include "algorithm/utils/operation.h"
+#include "algorithm.h"
 
 
-static void stockham_forward(const size_t n, const size_t s, const bool eo, ft_complex *x, ft_complex *y) {
-    if (n == 2) {
-        ft_complex *z = eo ? y : x;
+static void stockham_forward(const size_t n, ft_complex *sequence, const size_t thread_count = 1) {
+    auto *work_area = new ft_complex[n];
+    ft_complex *x = sequence;
+    ft_complex *y = work_area;
 
-        for (size_t q = 0; q < s; ++q) {
-            ft_complex a, b;
-            FT_COPY(x[q], a);
-            FT_COPY(x[q + s], b);
-            FT_ADD(a, b, z[q]);
-            FT_SUB(a, b, z[q + s]);
-        }
-    } else if (n > 2) {
-        const size_t half = n / 2;
+    for (size_t half = n >> 1, s = 1; half > 1; half >>= 1, s <<= 1) {
         const double theta = std::numbers::pi / static_cast<double>(half);
+        std::vector<std::thread> threads;
 
-        for (size_t p = 0; p < half; ++p) {
-            const double angle = static_cast<double>(p) * theta;
-            const ft_complex w = {std::cos(angle), -std::sin(angle)};
+        auto task = [&](const size_t t) {
+            const auto [start, end] = thread_range(half, t, thread_count);
 
-            for (size_t q = 0; q < s; ++q) {
-                ft_complex a, b;
-                FT_COPY(x[q + s * p], a);
-                FT_COPY(x[q + s * (p + half)], b);
-                FT_ADD(a, b, y[q + s * (2 * p)]);
-                FT_SUB(a, b, y[q + s * (2 * p + 1)]);
-                FT_RMUL(y[q + s * (2 * p + 1)], w);
+            for (size_t p = start; p < end; ++p) {
+                const double angle = static_cast<double>(p) * theta;
+                const ft_complex w = {std::cos(angle), -std::sin(angle)};
+
+                for (size_t q = 0; q < s; ++q) {
+                    ft_complex a, b;
+                    FT_COPY(x[q + s * p], a);
+                    FT_COPY(x[q + s * (p + half)], b);
+                    FT_ADD(a, b, y[q + s * (2 * p)]);
+                    FT_SUB(a, b, y[q + s * (2 * p + 1)]);
+                    FT_RMUL(y[q + s * (2 * p + 1)], w);
+                }
             }
+        };
+
+        for (size_t t = 1; t < thread_count; ++t) {
+            threads.emplace_back(task, t);
+        }
+        task(0);
+
+        for (auto &t: threads) {
+            t.join();
         }
 
-        stockham_forward(half, 2 * s, !eo, y, x);
+        std::swap(x, y);
     }
+
+    const size_t half = n >> 1;
+    for (size_t q = 0; q < half; ++q) {
+        ft_complex a, b;
+        FT_COPY(x[q], a);
+        FT_COPY(x[q + half], b);
+        FT_ADD(a, b, sequence[q]);
+        FT_SUB(a, b, sequence[q + half]);
+    }
+
+    delete[] work_area;
 }
 
-static void stockham_inverse(const size_t n, ft_complex *x, ft_complex *y) {
+static void stockham_inverse(const size_t n, ft_complex *sequence, const size_t thread_count = 1) {
     for (size_t i = 0; i < n; ++i) {
-        FT_RCONJ(x[i]);
+        FT_RCONJ(sequence[i]);
     }
 
-    stockham_forward(n, 1, false, x, y);
+    stockham_forward(n, sequence, thread_count);
 
     for (size_t i = 0; i < n; ++i) {
-        x[i][0] = x[i][0] / static_cast<double>(n);
-        x[i][1] = -x[i][1] / static_cast<double>(n);
+        sequence[i][0] = sequence[i][0] / static_cast<double>(n);
+        sequence[i][1] = -sequence[i][1] / static_cast<double>(n);
     }
 }
 
@@ -56,43 +74,57 @@ void Bluestein_I_Stockham_R2::initialize(const size_t n, ft_complex *in, ft_comp
     const size_t k = 2 * n - 1;
     for (l = 1; l < k; l <<= 1) {
     }
-
-    temp = new ft_complex[l];
-    u = new ft_complex[l];
-    v = new ft_complex[l];
 }
 
-void Bluestein_I_Stockham_R2::finalize(const size_t n, ft_complex *in, ft_complex *out) {
-    delete[] temp;
-    delete[] u;
-    delete[] v;
-}
-
-// TODO: Нет многопоточности
 void Bluestein_I_Stockham_R2::forward(const size_t n, ft_complex *in, ft_complex *out) {
-    for (size_t i = 0; i < n; ++i) {
-        FT_POLAR(-std::numbers::pi * i * (static_cast<double>(i) / n), out[i]);
-        FT_MUL(in[i], out[i], u[i]);
-        FT_CONJ(v[i], out[i]);
+    std::vector<std::thread> threads;
+    const size_t thread_count = get_max_threads();
+    auto *u = new ft_complex[l];
+    auto *v = new ft_complex[l];
+
+    auto task = [&](const size_t t) {
+        const auto [start, end] = thread_range(n, t, thread_count);
+        for (size_t i = start; i < end; ++i) {
+            FT_POLAR(-std::numbers::pi * i * (static_cast<double>(i) / n), out[i]);
+            FT_MUL(in[i], out[i], u[i]);
+            FT_CONJ(v[i], out[i]);
+        }
+    };
+
+    for (size_t t = 1; t < thread_count; ++t) {
+        threads.emplace_back(task, t);
     }
+    task(0);
+    for (auto &t: threads) {
+        t.join();
+    }
+
     for (size_t i = n; i < l; ++i) {
         FT_ZERO(u[i]);
     }
-
     for (size_t i = 1; i < n; ++i) {
         FT_COPY(v[i], v[l - i]);
     }
 
-    stockham_forward(l, 1, false, u, temp);
-    stockham_forward(l, 1, false, v, temp);
+    if (thread_count > 1) {
+        std::thread t(stockham_forward, l, u, std::max(1ul, thread_count / 2));
+        stockham_forward(l, v, std::max(1ul, thread_count / 2));
+        t.join();
+    } else {
+        stockham_forward(l, u);
+        stockham_forward(l, v);
+    }
 
     for (size_t i = 0; i < l; ++i) {
         FT_RMUL(u[i], v[i]);
     }
 
-    stockham_inverse(l, u, temp);
+    stockham_inverse(l, u, thread_count);
 
     for (size_t i = 0; i < n; ++i) {
         FT_RMUL(out[i], u[i]);
     }
+
+    delete[] u;
+    delete[] v;
 }
--- src/algorithm/impl/fft/any/Bluestein_I_Stockham_R4.h
+++ src/algorithm/impl/fft/any/Bluestein_I_Stockham_R4.h
@@ -6,9 +6,6 @@
 
 class Bluestein_I_Stockham_R4 final : public IFourierTransformAlgorithm {
     size_t l = 0;
-    ft_complex *temp = nullptr;
-    ft_complex *u = nullptr;
-    ft_complex *v = nullptr;
 
 public:
     static constexpr auto NAME = "B-I+S-4";
@@ -19,8 +16,6 @@ public:
 
 protected:
     void initialize(size_t n, ft_complex *in, ft_complex *out) override;
-
-    void finalize(size_t n, ft_complex *in, ft_complex *out) override;
 
     void forward(size_t n, ft_complex *in, ft_complex *out) override;
 };
